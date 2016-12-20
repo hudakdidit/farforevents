f476a789b7d5716557bebe2942c69f42664a6ffd
Jonathan Hudak <hudak.jonathan@gmail.com>
2016-12-20 13:54:19 -0800

    setup

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var path = require('path');
+var poststylus = require('poststylus');
+var autoprefixer = require('autoprefixer');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: './src/js/index',
+  },
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: '[name].js',
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.json']
+  },
+  module: {
+      loaders: [
+        { test: /\.json$/, loader: 'json'},
+        {
+          test: /\.jsx?$/,
+          exclude: /(node_modules)/,
+          loader: 'babel',
+          query: {
+            presets: ['es2015']
+          }
+        },
+        {
+          key: 'styl',
+          test:   /\.styl$/,
+          loader: "style!css!stylus"
+        }
+      ]
+  },
+  stylus: {
+    use: [
+      poststylus([ 'autoprefixer' ])
+    ]
+  }
+}
